316965992a130ef99e3c8ea3cda18f8cac0988fe
Jun Hyun Song <hyun8510@jun-ui-MacBook-Air.local>
2016-03-07 22:31:45 +0900

    OkNetworkManager Class 구현

---
+++ webcontentsviewer/app/src/main/java/com/junhyunsss/webcontentsviewer/networkmodule/networkmanager/OkNetworkManager.java
@@ -0,0 +1,61 @@
+package com.junhyunsss.webcontentsviewer.networkmodule.networkmanager;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+
+import com.junhyunsss.webcontentsviewer.networkmodule.NetworkRequest;
+import com.junhyunsss.webcontentsviewer.networkmodule.OnResultListener;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+
+import java.io.IOException;
+
+/**
+ * Created by junhyunss on 16. 3. 7..
+ */
+public class OkNetworkManager {
+    public static final int MESSAGE_SUCCESS = 0;
+    public static final int MESSAGE_FAIL = 1;
+    public static final int MESSAGE_CANCEL = 2;
+
+    private static OkNetworkManager sInstance = null;
+    public static OkNetworkManager getsInstance() {
+        if (sInstance == null) {
+            synchronized (OkNetworkManager.class) {
+                if (sInstance == null) {
+                    return new OkNetworkManager();
+                }
+            }
+        }
+        return sInstance;
+    }
+
+    private OkHttpClient mClient;
+
+    private OkNetworkManager () {
+        mClient = new OkHttpClient();
+    }
+
+    private static class OkResult<T> {
+        OnResultListener<T> listener;
+        Request request;
+        T result;
+        IOException e;
+    }
+
+    private Handler mHandler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(Message msg) {
+            NetworkRequest request = (NetworkRequest) msg.obj;
+            switch (msg.what) {
+                case MESSAGE_SUCCESS:
+                    request.sendSuccess();
+                    break;
+                case MESSAGE_FAIL:
+                    request.sendFail();;
+                    break;
+            }
+        }
+    };
+}
